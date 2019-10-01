4161793e44da1d8bfea6ab6bb228ba4f322c972e
ipal0 <ipal00@outlook.com>
2019-10-01 15:01:31 -0400

    updated with statement

--- __init__.py
+++ __init__.py
@@ -6,29 +6,29 @@ class db:
 		self.conn.autocommit = 1
 
 	def read(self, command, flat=True):
-		cur = self.conn.cursor()
-		cur.execute(command)
-		f = cur.fetchall()
-		if not f or not flat:
-			return f
-		else:
-			if len(f[0]) == 1:
-				f = [i[0] for i in f]
-				if len(f) == 1:
-					f = f[0]
-			return f
+		with self.conn.cursor() as cur:
+			cur.execute(command)
+			f = cur.fetchall()
+			if not f or not flat:
+				return f
+			else:
+				if len(f[0]) == 1:
+					f = [i[0] for i in f]
+					if len(f) == 1:
+						f = f[0]
+				return f
 
 	def read1(self, command):
-		cur = self.conn.cursor()
-		cur.execute(command)
-		f = cur.fetchone()
-		if len(f) == 1:
-			f = f[0]
-		return f
+		with self.conn.cursor() as cur:				
+			cur.execute(command)
+			f = cur.fetchone()
+			if len(f) == 1:
+				f = f[0]
+			return f
 
 	def write(self, command):
-		cur = self.conn.cursor()
-		cur.execute(command)
+		with self.conn.cursor() as cur:
+			cur.execute(command)
 
 	def __del__(self):
 		self.conn.close()
