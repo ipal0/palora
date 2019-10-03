82f16ff61bd060ca5394a72bf652cdd1a04fdb23
ipal0 <ipal00@outlook.com>
2019-10-02 20:44:07 -0400

    added multiple connections

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 ## Wrapper Class around cx_Oracle for Oracle Autonomous Database
+* This class supports multiple connections to the database using session pool. The minimum and maximum number of sessions can be defined. 
 * In cx_Oracle after connecting to the database, for every query statement the data needs to be fetched and for evey update or insert statement the data needs to be committed. But this class eliminates these requirements and make it simple to read and write to the database.
 
 # Files:
@@ -9,7 +10,7 @@
 
 ## Usage Examples:
 * import palora
-* db = palora.db('user', 'password', 'tsn-entry')  ##-connect to the database as user
+* db = palora.db('user', 'password', 'tsn-entry')  ##-connect to the database with user, password, tsn. Optional parameters are min, max, increment & encoding.
 * db.read("sql statement")  ##-returns the read data from database with flat=True
 * db.read("select item from table", flat=False)  ##-returns the raw read data 
 * db.read("select item from table where item = %s", (sub1,))  ##-read with string substitution
--- __init__.py
+++ __init__.py
@@ -2,34 +2,38 @@ import cx_Oracle
 
 class db:
 	def __init__(self, user, pw, tns):
-		self.conn = cx_Oracle.connect(user, pw, tns)
-		self.conn.autocommit = 1
+		self.pool = cx_Oracle.SessionPool(user, pw, tns, min=2, max=20, increment=1, encoding="UTF-8")
 
 	def read(self, command, flat=True):
-		with self.conn.cursor() as cur:
-			cur.execute(command)
-			f = cur.fetchall()
-			if not f or not flat:
-				return f
-			else:
-				if len(f[0]) == 1:
-					f = [i[0] for i in f]
-					if len(f) == 1:
-						f = f[0]
-				return f
-
-	def read1(self, command):
-		with self.conn.cursor() as cur:				
-			cur.execute(command)
-			f = cur.fetchone()
+		con = self.pool.acquire()
+		cur = con.cursor()
+		f = cur.execute(command).fetchall()
+		self.pool.release(con)
+		if not f or not flat:
+			return f
+		else:
+			if len(f[0]) == 1:
+				f = [i[0] for i in f]
 			if len(f) == 1:
-				f = f[0]
+					f = f[0]
 			return f
 
+	def read1(self, command):
+		con = self.pool.acquire()
+		cur = con.cursor()
+		f = cur.execute(command).fetchone()
+		self.pool.release(con)
+		if len(f) == 1:
+			f = f[0]
+		return f
+
 	def write(self, command):
-		with self.conn.cursor() as cur:
-			cur.execute(command)
+		con = self.pool.acquire()
+		cur = con.cursor()
+		cur.execute(command)
+		con.commit()
+		self.pool.release(con)
 
 	def __del__(self):
-		self.conn.close()
+		self.pool.close()
 
